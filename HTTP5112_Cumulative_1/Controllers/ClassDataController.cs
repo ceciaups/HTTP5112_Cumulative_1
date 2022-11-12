7839028b1538f0eb4ef1ea70582ce846210efea6
cecidiot777 <cecilia127@msn.com>
2022-11-12 14:36:03 -0500

    Create Project

---
+++ HTTP5112_Cumulative_1/Controllers/ClassDataController.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using HTTP5112_Cumulative_1.Models;
+using MySql.Data.MySqlClient;
+
+namespace HTTP5112_Cumulative_1.Controllers
+{
+    public class ClassDataController : Controller
+    {
+        private SchoolDbContext School = new SchoolDbContext();
+
+        [HttpGet]
+        [Route("api/ClassData/ListClasses")]
+        public IEnumerable<Class> ListClasses()
+        {
+            MySqlConnection Conn = School.AccessDatabase();
+
+            Conn.Open();
+
+            MySqlCommand cmd = Conn.CreateCommand();
+
+            cmd.CommandText = "SELECT * FROM classes";
+
+            MySqlDataReader ResultSet = cmd.ExecuteReader();
+
+            List<Class> Classes = new List<Class> { };
+
+            while (ResultSet.Read())
+            {
+                int ClassId = Convert.ToInt32(ResultSet["classid"]);
+                string ClassCode = ResultSet["classcode"].ToString();
+                int TeacherId = Convert.ToInt32(ResultSet["teacherid"]);
+                string StartDate = ResultSet["startdate"].ToString();
+                string FinishDate = ResultSet["finishdate"].ToString();
+                string ClassName = ResultSet["classname"].ToString();
+
+                Class MyClass = new Class();
+                MyClass.ClassId = ClassId;
+                MyClass.ClassCode = ClassCode;
+                MyClass.TeacherId = TeacherId;
+                MyClass.StartDate = StartDate;
+                MyClass.FinishDate = FinishDate;
+                MyClass.ClassName = ClassName;
+
+                Classes.Add(MyClass);
+            }
+
+            Conn.Close();
+
+            return Classes;
+        }
+
+        [HttpGet]
+        [Route("api/ClassData/FindClass")]
+        public Class FindClass(int id)
+        {
+            Class NewClass = new Class();
+
+            MySqlConnection Conn = School.AccessDatabase();
+
+            Conn.Open();
+
+            MySqlCommand cmd = Conn.CreateCommand();
+
+            cmd.CommandText = "SELECT * FROM classes WHERE classid = " + id;
+
+            MySqlDataReader ResultSet = cmd.ExecuteReader();
+
+            while (ResultSet.Read())
+            {
+                int ClassId = Convert.ToInt32(ResultSet["classid"]);
+                string ClassCode = ResultSet["classcode"].ToString();
+                int TeacherId = Convert.ToInt32(ResultSet["teacherid"]);
+                string StartDate = ResultSet["startdate"].ToString();
+                string FinishDate = ResultSet["finishdate"].ToString();
+                string ClassName = ResultSet["classname"].ToString();
+
+                NewClass.ClassId = ClassId;
+                NewClass.ClassCode = ClassCode;
+                NewClass.TeacherId = TeacherId;
+                NewClass.StartDate = StartDate;
+                NewClass.FinishDate = FinishDate;
+                NewClass.ClassName = ClassName;
+            }
+
+            Conn.Close();
+
+            return NewClass;
+        }
+    }
+}
+
---
+++ HTTP5112_Cumulative_1/Controllers/TeacherDataController.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using HTTP5112_Cumulative_1.Models;
+using MySql.Data.MySqlClient;
+
+namespace HTTP5112_Cumulative_1.Controllers
+{
+    public class TeacherDataController : Controller
+    {
+        private SchoolDbContext School = new SchoolDbContext();
+
+        [HttpGet]
+        [Route("api/TeacherData/ListTeachers")]
+        public IEnumerable<Teacher> ListTeachers()
+        {
+            MySqlConnection Conn = School.AccessDatabase();
+
+            Conn.Open();
+
+            MySqlCommand cmd = Conn.CreateCommand();
+
+            cmd.CommandText = "SELECT * FROM teachers";
+
+            MySqlDataReader ResultSet = cmd.ExecuteReader();
+
+            List<Teacher> Teachers = new List<Teacher> { };
+
+            while (ResultSet.Read())
+            {
+                int TeacherId = Convert.ToInt32(ResultSet["teacherid"]);
+                string TeacherFname = ResultSet["teacherfname"].ToString();
+                string TeacherLname = ResultSet["teacherlname"].ToString();
+                string EmployeeNumber = ResultSet["employeenumber"].ToString();
+                string HireDate = ResultSet["hiredate"].ToString();
+                decimal Salary = Convert.ToDecimal(ResultSet["salary"]);
+
+                Teacher MyTeacher = new Teacher();
+                MyTeacher.TeacherId = TeacherId;
+                MyTeacher.TeacherFname = TeacherFname;
+                MyTeacher.TeacherLname = TeacherLname;
+                MyTeacher.EmployeeNumber = EmployeeNumber;
+                MyTeacher.HireDate = HireDate;
+                MyTeacher.Salary = Salary;
+
+                Teachers.Add(MyTeacher);
+            }
+
+            Conn.Close();
+
+            return Teachers;
+        }
+
+        [HttpGet]
+        [Route("api/TeacherData/FindTeacher")]
+        public Teacher FindTeacher(int id)
+        {
+            Teacher NewTeacher = new Teacher();
+
+            MySqlConnection Conn = School.AccessDatabase();
+
+            Conn.Open();
+
+            MySqlCommand cmd = Conn.CreateCommand();
+
+            cmd.CommandText = "SELECT * FROM teachers WHERE teacherid = " + id;
+
+            MySqlDataReader ResultSet = cmd.ExecuteReader();
+
+            while (ResultSet.Read())
+            {
+                int TeacherId = Convert.ToInt32(ResultSet["teacherid"]);
+                string TeacherFname = ResultSet["teacherfname"].ToString();
+                string TeacherLname = ResultSet["teacherlname"].ToString();
+                string EmployeeNumber = ResultSet["employeenumber"].ToString();
+                string HireDate = ResultSet["hiredate"].ToString();
+                decimal Salary = Convert.ToDecimal(ResultSet["salary"]);
+
+                NewTeacher.TeacherId = TeacherId;
+                NewTeacher.TeacherFname = TeacherFname;
+                NewTeacher.TeacherLname = TeacherLname;
+                NewTeacher.EmployeeNumber = EmployeeNumber;
+                NewTeacher.HireDate = HireDate;
+                NewTeacher.Salary = Salary;
+            }
+
+            Conn.Close();
+
+            return NewTeacher;
+        }
+    }
+}
+
