0049bee8569efabe2dc9570ce092ebcf965b04f9
ceciaups <cecilia127@msn.com>
2022-11-12 15:05:09 -0500

    Build MVP on Students table

---
+++ HTTP5112_Cumulative_1/Controllers/StudentDataController.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using HTTP5112_Cumulative_1.Models;
+using MySql.Data.MySqlClient;
+
+namespace HTTP5112_Cumulative_1.Controllers
+{
+    public class StudentDataController : Controller
+    {
+        private SchoolDbContext School = new SchoolDbContext();
+
+        [HttpGet]
+        [Route("api/StudentData/ListStudents")]
+        public IEnumerable<Student> ListStudents()
+        {
+            MySqlConnection Conn = School.AccessDatabase();
+
+            Conn.Open();
+
+            MySqlCommand cmd = Conn.CreateCommand();
+
+            cmd.CommandText = "SELECT * FROM students";
+
+            MySqlDataReader ResultSet = cmd.ExecuteReader();
+
+            List<Student> Students = new List<Student> { };
+
+            while (ResultSet.Read())
+            {
+                int StudentId = Convert.ToInt32(ResultSet["studentid"]);
+                string StudentFname = ResultSet["studentfname"].ToString();
+                string StudentLname = ResultSet["studentlname"].ToString();
+                string StudentNumber = ResultSet["studentnumber"].ToString();
+                string EnrolDate = ResultSet["enroldate"].ToString();
+
+                Student MyStudent = new Student();
+                MyStudent.StudentId = StudentId;
+                MyStudent.StudentFname = StudentFname;
+                MyStudent.StudentLname = StudentLname;
+                MyStudent.StudentNumber = StudentNumber;
+                MyStudent.EnrolDate = EnrolDate;
+
+                Students.Add(MyStudent);
+            }
+
+            Conn.Close();
+
+            return Students;
+        }
+
+        [HttpGet]
+        [Route("api/StudentData/FindStudent")]
+        public Student FindStudent(int id)
+        {
+            Student NewStudent = new Student();
+
+            MySqlConnection Conn = School.AccessDatabase();
+
+            Conn.Open();
+
+            MySqlCommand cmd = Conn.CreateCommand();
+
+            cmd.CommandText = "SELECT * FROM Students WHERE Studentid = " + id;
+
+            MySqlDataReader ResultSet = cmd.ExecuteReader();
+
+            while (ResultSet.Read())
+            {
+                int StudentId = Convert.ToInt32(ResultSet["studentid"]);
+                string StudentFname = ResultSet["studentfname"].ToString();
+                string StudentLname = ResultSet["studentlname"].ToString();
+                string StudentNumber = ResultSet["studentnumber"].ToString();
+                string EnrolDate = ResultSet["enroldate"].ToString();
+
+                NewStudent.StudentId = StudentId;
+                NewStudent.StudentFname = StudentFname;
+                NewStudent.StudentLname = StudentLname;
+                NewStudent.StudentNumber = StudentNumber;
+                NewStudent.EnrolDate = EnrolDate;
+            }
+
+            Conn.Close();
+
+            return NewStudent;
+        }
+    }
+}
+
